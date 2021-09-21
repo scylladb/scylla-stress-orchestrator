90f92b6dea9c7a3ebe32dec19304f34d0a2a14af
Peter Veentjer <alarmnummer@gmail.com>
2021-09-21 09:27:37 +0300

    Added warmup_seconds to cassandra_stress template.
    Now it is easier to the end user to trim the warmup period.

--- template/cassandra-stress.py
+++ template/cassandra-stress.py
@@ -35,7 +35,9 @@ cs.prepare()
 items = 10_000_000
 
 # The running time of the benchmark
-duration = "2m"
+duration = "12m"
+# 2 minutes of warmup are removed from the duration.
+warmup_seconds = 120 
 
 # Configuration for throughput test 
 rate = f'threads=200'
@@ -55,7 +57,7 @@ scylla.restart_cluster(env['cluster_public_ips'], props['cluster_user'], props['
 cs.stress(f'user profile=./stress_example.yaml "ops(insert=1)" duration={duration} -pop seq=1..{items} -log hdrfile=profile.hdr -graph file=report.html title=benchmark revision=benchmark-0 -mode native cql3 -rate {rate} -node {cluster_string}')  
 
 # collect the results.
-cs.collect_results(iteration.dir)
+cs.collect_results(iteration.dir, warmup_seconds = warmup_seconds)
 
 # Download and clear the prometheus data (can take a lot of time/space)
 prometheus.download_and_clear(env, props, iteration)
