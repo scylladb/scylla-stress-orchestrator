1248074e52d91870dbd2e937febcd2641b020fab
Peter Veentjer <alarmnummer@gmail.com>
2021-10-05 11:34:27 +0300

    Made scylla version configurable in scylla monitoring.
    Also resolved a file name problem.

--- scyllaso/prometheus.py
+++ scyllaso/prometheus.py
@@ -23,11 +23,12 @@ def download_and_clear(env, props, iteration):
 
 class Prometheus:
 
-    def __init__(self, ip, user, ssh_options, scylla_monitoring_version="3.8"):
+    def __init__(self, ip, user, ssh_options, scylla_monitoring_version="3.8.2", scylla_version="4.4"):
         self.ip = ip
         self.user = user
         self.ssh_options = ssh_options
         self.scylla_monitoring_version = scylla_monitoring_version
+        self.scylla_version = scylla_version
 
     def data_dir_upload(self, dir):
         log_important("Prometheus upload: started")
@@ -40,7 +41,7 @@ class Prometheus:
         ssh = SSH(self.ip, self.user, self.ssh_options)
         ssh.exec(
             f"""
-            cd scylla-monitoring-scylla-monitoring-{self.scylla_monitoring_version}
+            cd scylla-monitoring-{self.scylla_monitoring_version}
             ./kill-all.sh
             """)
         log_important("Prometheus stop: done")
@@ -51,8 +52,8 @@ class Prometheus:
         ssh.exec(
             f"""
             mkdir -p data
-            cd scylla-monitoring-scylla-monitoring-{self.scylla_monitoring_version}
-            ./start-all.sh -v 4.3 -d ../data
+            cd scylla-monitoring-{self.scylla_monitoring_version}
+            ./start-all.sh -v {self.scylla_version} -d ../data
             """)
         log_important("Prometheus start: done")
 
