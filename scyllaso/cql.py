dabdda785f076dda1f25632f550582caf8081007
Peter Veentjer <alarmnummer@gmail.com>
2021-12-13 11:04:37 +0200

    cqlsh improvements

--- scyllaso/cql.py
+++ scyllaso/cql.py
@@ -5,16 +5,19 @@ import socket
 from scyllaso.ssh import SSH
 from scyllaso.util import log_machine, log_important, run_parallel
 
-
+#
+# Contains the 'cqlsh' abstraction that executes CQL commands on some remote
+# node using SSH.
+#
 class cqlsh:
 
-    def __init__(self, ip_list, ssh_user, ssh_options, username=None, password=None):
-        self.ip_list = ip_list
+    def __init__(self, ip, ssh_user, ssh_options, username=None, password=None):
+        self.ip = ip
         self.ssh_user = ssh_user
         self.ssh_options = ssh_options
         self.username = username
         self.password = password
-        self.wait_for_cql_start()
+        self.started = False
 
     def __new_ssh(self, ip):
         return SSH(ip, self.ssh_user, self.ssh_options)
@@ -27,21 +30,31 @@ class cqlsh:
         run_parallel(self.__wait_for_cql_start, [(ip, timeout, connect_timeout, max_tries_per_second) for ip in self.ip_list])
         log_important(f"cqlsh: running")
 
-    def __exec(self, ip, cql):
-        ssh = self.__new_ssh(ip)
+    def exec(self, cql):
+        """
+        Executes a cql command.
+
+        Parameters
+        ----------
+        cql: str
+            The CQL command
+        """
+
+        if not self.started:
+            self.wait_for_cql_start()
+            self.started = True
+
+        log_important(f"cqlsh exec: [{cql}]")
+        ssh = self.__new_ssh(self.ip)
         ssh.exec("touch foo.cql")
         ssh.exec(f"echo \"{cql}\" > foo.cql")
-        cmd ="cqlsh "
+        cmd = "cqlsh "
         if self.username:
-            cmd+=f"-u {self.username} "
+            cmd += f"-u {self.username} "
         if self.password:
             cmd += f"-p {self.password} "
         cmd += "-f foo.cql"
         ssh.exec(cmd)
-
-    def exec(self, cql):
-        log_important(f"cqlsh exec: [{cql}]")
-        run_parallel(self.__exec, [(ip,cql) for ip in self.ip_list])
         log_important(f"cqlsh done")
 
 
